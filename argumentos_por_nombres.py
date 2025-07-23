dd4c0414e415079639a04f9a5a5b994ae77f532f
matikof <matikof2014@gmail.com>
2025-07-23 15:43:50 -0300

    Terminamos la leccion de argumentos por nombres en Python

--- argumentos_por_nombres.py
+++ argumentos_por_nombres.py
@@ -1,1 +1,17 @@
 print("*** Funcion con argumentos por nombre ***")
+#Esto se hace cuando pasamos valores a una funcion
+#y ahi especificamos que tipo de variable vamos a pasar en lugar
+#de simplemente usar la posición
+
+def imprimir_persona(nombre , apellido, edad):
+    print(f"Persona: Nombre = {nombre}, Apellido = {apellido}, Edad ={edad}")
+
+#Primero llamamos a la funcion usando el metodo normal por posicion
+imprimir_persona("Matías", "Torres", 32)
+#Ahora llamamos a la funcion usando argumento por nombres.
+imprimir_persona(edad=40, nombre="Jose", apellido="jijo")
+
+
+def imprimir_personas_dos(nombre, apellido = "", edad = ""):
+    print(f"Persona: Nombre = {nombre}, Apellido = {apellido}, Edad = {edad}")
+
