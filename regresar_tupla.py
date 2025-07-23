fa8e64e110419f9a01f090dc2bfdbb3fc9907348
matikof <matikof2014@gmail.com>
2025-07-23 18:27:23 -0300

    Terminamos lo que es el como regresar varios valores en un return de una función

--- regresar_tupla.py
+++ regresar_tupla.py
@@ -1,2 +1,15 @@
 #Regrasamos una tupla mediante una función
 
+print("*** Regresar una tupla de valores desde una función ***")
+
+#Definición de funcion
+
+def persona_mayusculas(nombre, apellido, edad):
+    print(f"Esta función regresa varios valores (tupla)")
+    return (nombre.upper(), apellido.upper(), edad)
+
+#LLamamos a una función para asignar cada valor
+#a cada variable usamos unpacking(desempaquetado)
+
+nombre, apellido , edad = persona_mayusculas("Matías","Torres", 32)
+print(f"Los datos de la Persona: nombre = {nombre}, apellido = {apellido}, edad = {edad}")
