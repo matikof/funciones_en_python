1e4d3f175fe5ade87ede0fc913cee031262f9eb8
matikof <matikof2014@gmail.com>
2025-07-23 18:40:44 -0300

    Terminamos con el ejemplo de coordenadas

---
+++ ejemplo_coordenadas.py
@@ -0,0 +1,9 @@
+print("*** Obtener coordenadas x,y,z")
+
+def obtener_coordenadas():
+    x, y, z = 10, 20,30
+    return (x, y, z)
+#llamar la funcion
+#resultado = obtener_coordenadas()
+x,y,z = obtener_coordenadas()
+print(f"Coordenadas: X = {x}, Y = {y}, Z = {z}")
