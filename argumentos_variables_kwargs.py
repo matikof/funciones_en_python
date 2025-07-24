5b59b991926f74b29c426b7d1efd7fb0c4ebfc04
matikof <matikof2014@gmail.com>
2025-07-23 23:39:00 -0300

    Terminamos con la lección de Argumentos Variables Kwargs

--- argumentos_variables_kwargs.py
+++ argumentos_variables_kwargs.py
@@ -1,1 +1,7 @@
-print()
+print("*** Argumentos Variables Kwargs ***")
+
+def superheroe_superpoderes(nombre, *args, **kwargs):
+    print(f"Nombre: {nombre}, Poderes(tupla): {args}, Mas Info: {kwargs}")
+
+#Ingresamos
+superheroe_superpoderes("SpiderMan", "Sentido Aracnido","Telaraña", nombre_real = "Peter Parker", pareja = "Mary Jane")
