195a5166fa1dc37d5482b58f968ae07550629d33
matikof <matikof2014@gmail.com>
2025-07-24 18:15:43 -0300

    Terminamos con el tema de argumentos variables

--- ejercicios_argumentos_variables.py
+++ ejercicios_argumentos_variables.py
@@ -1,7 +1,5 @@
 print("*** Varios Ejercicios con Argumentos Variables ***")
-
 #Función sumar que acepta una tupla(*args)
-
 def sumar(*args):
     total = 0
     for numero in args:
@@ -9,3 +7,24 @@ def sumar(*args):
     return total
 resultado = sumar(1,2,4,5,3)
 print(f"Resultado de la Suma: {resultado}")
+
+print("--- Ejemplo con **kwargs ---")
+def imprimir_detalle_persona(**kwargs):
+    print("\nDetalle Persona:")
+    #Al usar .items() devolvemos una tupla
+    for llave, valor in kwargs.items():
+        print(f"{llave}:{valor}")
+
+imprimir_detalle_persona(nombre="Matías", edad = 32, ciudad = "Moreno")
+
+#funcion par
+def es_par(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+#Con esto solo dejamos ejecutar esto desde este mismo archivo
+#y no desde uno externo
+if __name__ == "__main__":
+    numero = int(input("Proporciona un valor númerico: "))
+    print(f"Es un número par? {es_par(numero)}")
