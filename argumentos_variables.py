68a3ff424a4cb0afb5da6b382fb1726a11068bae
matikof <matikof2014@gmail.com>
2025-07-23 21:28:26 -0300

    Usamos *args y finalizamos esa parte

--- argumentos_variables.py
+++ argumentos_variables.py
@@ -1,1 +1,17 @@
-print("*** Argumentos en Java ***")
+print("*** Argumentos Variables en Java ***")
+#Definimos una función la cual va a pedir argumentos posicionales
+#Pero también va a tener la posibilidad de ingresar argumentos
+#variables
+def superheroe_superpoder(superheroe, nombre, *args):
+    print(f"SuperHeroe: {superheroe} -> Nombre Real: {nombre} -> {args}")
+
+superheroe_superpoder("SpiderMan", "Peter Parker", "Instinto Aracnido", "Telaraña")
+
+#Volvemos a llamar a la función para comprobar que se pueden pasar
+#una cantidad cualquiera de argumentos(incluso ninguno)
+superheroe_superpoder("Batman", "Bruce Wayne", "Millonario", "Traje","Artefactos")
+#¡¡¡¡Importante!!!!
+#   *args debe pedirse al final de la peticion de argumentos
+#   en la función, ya que como tenemos argumentos posicionales
+#   nos dará un error por no pasarlos.
+
